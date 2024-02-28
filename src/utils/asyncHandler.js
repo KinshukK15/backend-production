e69f16b759e245941e9ef30588fcff79a9f9046c
KinshukK15 <kinshuk1563@gmail.com>
2024-02-28 16:12:28 +0530

    all setup configured

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,3 +1,12 @@
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+
+export { asyncHandler }
+
 // const asyncHandler = (fn) => async (req, res, next) => {
 //     try {
 //         await fn(req, res, next)
@@ -8,13 +17,3 @@
 //         })
 //     }
 // }
-
-
-const asyncHandler = (requestHandler) => {
-    (req, res, next) => {
-        Promise.resolve(requestHandler(req, res, next)).
-        catch((err)=> next(err))
-    }
-}
-
-export {asyncHandler}
