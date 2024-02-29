846829520ef8456dcac6e2dbbba41d8e2b3c71ab
KinshukK15 <kinshuk1563@gmail.com>
2024-02-29 22:10:37 +0530

    fix all register controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,12 +1,11 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { ApiError } from "../utils/ApiError.js";
-import { User } from "../models/user.model.js"
-import { uploadOnCloudinary} from "../utils/cloudinary.js"
+import {ApiError} from "../utils/ApiError.js"
+import { User} from "../models/user.model.js"
+import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
 
-
 const registerUser = asyncHandler( async (req, res) => {
-      // get user details from frontend
+    // get user details from frontend
     // validation - not empty
     // check if user already exists: username, email
     // check for images, check for avatar
@@ -16,48 +15,53 @@ const registerUser = asyncHandler( async (req, res) => {
     // check for user creation
     // return res
 
-    const {fullName, email, username, password} = req.body
-    console.log("email: ", email)
 
+    const {fullName, email, username, password } = req.body
+    //console.log("email: ", email);
 
     if (
-        [fullName, email, username, password].some((field)=> 
-        field?.trim() === "" )
+        [fullName, email, username, password].some((field) => field?.trim() === "")
     ) {
-        throw new ApiError(400, "All fields are required") 
+        throw new ApiError(400, "All fields are required")
     }
 
-
-    const existedUser = User.findOne({
-        $or: [{username}, {email}]
+    const existedUser = await User.findOne({
+        $or: [{ username }, { email }]
     })
 
     if (existedUser) {
-        throw new ApiError(400, "User with email or username already exists")
+        throw new ApiError(409, "User with email or username already exists")
     }
-
+    console.log(req.files);
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    //const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
+    
 
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required")
     }
 
-    const avatar = await uploadOnCloudinary(avatarLocalPath);
-    const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
     if (!avatar) {
         throw new ApiError(400, "Avatar file is required")
     }
+   
 
     const user = await User.create({
-        fullName, 
+        fullName,
         avatar: avatar.url,
         coverImage: coverImage?.url || "",
-        email,
-        username: username.toLowerCase(),
-        password
+        email, 
+        password,
+        username: username.toLowerCase()
     })
 
     const createdUser = await User.findById(user._id).select(
@@ -69,9 +73,8 @@ const registerUser = asyncHandler( async (req, res) => {
     }
 
     return res.status(201).json(
-       new ApiResponse(200, createdUser, "user created successfully")
+        new ApiResponse(200, createdUser, "User registered Successfully")
     )
-
 
 } )
 
--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,23 +1,23 @@
-class ApiError extends Error{
+class ApiError extends Error {
     constructor(
         statusCode,
-        message = "Soemthing wwent",
-        error = [],
+        message= "Something went wrong",
+        errors = [],
         stack = ""
     ){
         super(message)
         this.statusCode = statusCode
         this.data = null
         this.message = message
-        this.success = fasle;
+        this.success = false;
         this.errors = errors
 
-
-        if(stack){
-            this.stack =stack
+        if (stack) {
+            this.stack = stack
         } else{
             Error.captureStackTrace(this, this.constructor)
         }
+
     }
 }
 
